7d0f39252720cc8116ca9327b2c34a6e1d966d19
Staynall1999 <80275835+Staynall1999@users.noreply.github.com>
2023-02-12 18:17:29 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "02.02.2022"; // Дата рождения
+var fullname = "Фамилия Имя Отчество" // ФИО
+var name = "Имя" // Имя
+
+// Паспорт
+var passport_id = "323232323" // Номер паспорта
+
+// КПП
+var kpp_id = "1234567890" // ИНН
+
+// Права
+var rights_valid_until = "03.03.2023" // Права "Дійсні до"
+var rights_categories = "B, С" // Права "Категорії"
+var rights_tsc = "ТСЦ 3212" // Права "Видав
+var rights_id = "KBЕ313131" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "04.04.2024" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
